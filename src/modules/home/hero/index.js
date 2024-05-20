73a4b1ebcd7cd259a603ff3d3aa710c6340a087e
Aditto dev <computerscience01770@gmail.com>
2024-05-21 00:13:40 +1200

    [DONE]<Navbar>

--- src/modules/home/hero/index.js
+++ src/modules/home/hero/index.js
@@ -1,12 +1,73 @@
 import AboutHome from "../about";
 import styles from "./hero.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import o_image from "../../../assets/image/Hero/O.Png";
+import bg_image from "../../../assets/image/Hero/Wireframe-Vector.Png";
+import Arrow_icon from "../../../assets/icons/Arrow-Icon.Png";
 
 export default function Hero() {
   return (
     <>
-      <div>Hero</div>
-      {/* Home Hero section */}
-      <AboutHome />
+      <section className={styles.heroContainer}>
+        <div className={styles.heroAllTitleContainer}>
+          <div className={styles.heroFirstBoxStyle}></div>
+
+          <div className={styles.herSecondoBoxStyle}></div>
+
+          <div className={styles.heroFirstTitleContainer}>
+            <h1 className={styles.heroFirstTitleText}>
+              Your{" "}
+              <div>
+                <Image
+                  className={styles.heroOImage}
+                  src={o_image}
+                  width={100}
+                  height={100}
+                  alt="Picture of the author"
+                />
+                ne
+              </div>
+            </h1>
+
+            <p className={styles.heroContainerSubTitle}>
+              Create your future <br /> IT business with <br /> WaveNet.
+            </p>
+          </div>
+
+          <div className={styles.heroSecondTitleContainer}>
+            <h1 className={styles.heroSecondTitleText}>
+              <span className={styles.textItalic}> It</span>{" "}
+              <span className={styles.textTransparentColor}>Solutions</span>
+            </h1>
+
+            <div>
+              <Link href="/explore">
+                <button className={styles.heroSecondSubTitleBtn}>
+                  Explore now
+                  <Image
+                    src={Arrow_icon}
+                    width={90}
+                    height={0}
+                    alt="explore now arrow icon"
+                  />
+                </button>
+              </Link>
+            </div>
+          </div>
+
+          <div className={styles.heroBg}>
+            <Image
+              src={bg_image}
+              width={1900}
+              height={1000}
+              alt="Picture of the author"
+            />
+          </div>
+        </div>
+        {/* Home Hero section */}
+        <AboutHome />
+      </section>
     </>
   );
 }
--- src/shared/layout/navbar/navbar.js
+++ src/shared/layout/navbar/navbar.js
@@ -1,10 +1,53 @@
 import styles from "./navbar.module.css";
-
+import Link from "next/link";
+import Image from "next/image";
+import ecoLogo from "../../../assets/logo/Navbar/ecoysoft-logo.Png";
 export default function Navbar() {
+  const navBarItems = [
+    { itemName: "Home", url: "/" },
+    { itemName: "About Us", url: "/about" },
+    { itemName: "Services", url: "/services" },
+    { itemName: "Pricing", url: "/pricing" },
+    { itemName: "Blog", url: "/blog" },
+    { itemName: "Contact", url: "/contact" },
+  ];
+
   return (
     <>
-      <div>Navbar</div>
-      {/* Implement here the Navbar design */}
+      <section className={styles.navBarContaine}>
+        <nav className={styles.navBarAllItemsContainer}>
+          <div>
+            <li>
+              <Link className={styles.navBarItems} href="/">
+                <Image
+                  src={ecoLogo}
+                  width={180}
+                  height={100}
+                  alt="Picture of the author"
+                />
+              </Link>
+            </li>
+          </div>
+
+          <div>
+            <ul className={styles.navBarItemsContainer}>
+              {navBarItems.map((item, ind) => (
+                <li key={ind}>
+                  <Link className={styles.navBarItems} href={item.url}>
+                    {item.itemName}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div>
+            <Link href="/signIn">
+              <button className={styles.navBarTalkBtn}>Let's Talk</button>
+            </Link>
+          </div>
+        </nav>
+      </section>
     </>
   );
 }
