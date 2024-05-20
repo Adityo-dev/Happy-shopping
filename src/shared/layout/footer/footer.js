80788f12bd59f4af56358ee984fa665c816c9cdc
Aditto dev <computerscience01770@gmail.com>
2024-05-21 01:34:10 +1200

    [DONE]<Navbar> and hero section

--- src/shared/layout/footer/footer.js
+++ src/shared/layout/footer/footer.js
@@ -1,5 +1,32 @@
 import styles from "./footer.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div>
+        <div>
+          <h2>Need it solutions ?</h2>
+          <h2>
+            <span>Let's</span> <span>start now .</span>
+          </h2>
+        </div>
+
+        <div>
+          <Link href="/explore">
+            <button className={styles.heroSecondSubTitleBtn}>
+              Explore now
+              <Image
+                src={"/assets/icons/Arrow-Icon.Png"}
+                width={90}
+                height={0}
+                alt="explore now arrow icon"
+              />
+            </button>
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
 }
