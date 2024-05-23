53bbf3ecc2bc1bd4530442abebbfffe3a2796251
Aditto dev <computerscience01770@gmail.com>
2024-05-24 05:10:48 +1200

    [DONE]<blogs>

---
+++ src/modules/home/WhoWeAre/WhoWeAre.js
@@ -0,0 +1,92 @@
+import React from "react";
+import styles from "./whoWeAre.module.css";
+import Link from "next/link";
+import Image from "next/image";
+// IMPORT IMAGE
+import WhoWeAre_img from "../../../assets/image/WhoWeAre/WhoWeAre_img.png";
+import vector1_img from "../../../assets/image/WhoWeAre/vector_image.png";
+import vector2_img from "../../../assets/logo/WhoWeAre/Vector.png";
+import Success_img from "../../../assets/logo/WhoWeAre/Success-icon.png";
+// IMPORT REACT ICONS
+import { GoCheck } from "react-icons/go";
+
+export default function WhoWeAre() {
+  const items = [
+    "Strategy Consulting",
+    "Mistakes To Avoid",
+    "Analytics and Reporting",
+    "Your Startup",
+  ];
+  return (
+    <section className={styles.whoWeAreAllDataContainer}>
+      <div className={styles.whoWeAreDataContainer}>
+        <div>
+          <Image
+            className={styles.whoWeAreImage}
+            src={WhoWeAre_img}
+            width={592}
+            height={479}
+          />
+          <Image
+            className={styles.whoWeAreImageVector1}
+            src={vector2_img}
+            width={100}
+            height={100}
+          />
+          <div className={styles.whoWeAreImageSuccessContainer}>
+            <Image
+              className={styles.whoWeAreImageSuccessImg}
+              src={Success_img}
+              width={44}
+              height={62}
+            />
+            <p className={styles.whoWeAreImageSuccessNumText}>200+</p>
+            <p>Awards Winners </p>
+          </div>
+        </div>
+
+        <div className={styles.whoWeAreTextDataContainer}>
+          <div className={styles.whoWeAreTextContainer}>
+            <p className={styles.whoWeAreHrLine}></p>
+            <p className={styles.whoWeAreText}>Who we are</p>
+          </div>
+          <h3 className={styles.whoWeAreTitleText}>
+            Designing dreams, building brands
+          </h3>
+          <p className={styles.whoWeAreSubTitleText}>
+            It is a long established fact that a reader will be distracted by le
+            content of a page when looking at its layout. Lorem Ipsum is simply
+            dummy tex
+          </p>
+
+          <p className={styles.whoWeAreSubTitleText}>
+            It is a long established fact that a reader will be distracted bye
+            content of a page when looking at its layout.
+          </p>
+
+          <div className={styles.whoWeAreItemsListContainer}>
+            {items.map((item, ind) => (
+              <div className={styles.whoWeAreItemsContainer} key={ind}>
+                <GoCheck className={styles.whoWeAreItemsCheckIcon} />
+                <p className={styles.whoWeAreItemsText}>{item}</p>
+              </div>
+            ))}
+          </div>
+
+          <Link className={styles.whoWeAreReadMoreBtn} href={"/"}>
+            Read more
+          </Link>
+        </div>
+      </div>
+
+      <div>
+        <Image
+          className={styles.whoWeAreImageVector2}
+          src={vector1_img}
+          width={690}
+          height={690}
+        />
+      </div>
+    </section>
+  );
+}
--- src/modules/home/services/index.js
+++ src/modules/home/services/index.js
@@ -8,6 +8,7 @@ import security_icon from "../../../assets/logo/Services/security.png";
 // IMPORT REACT ICONS
 import { MdArrowForward } from "react-icons/md";
 import { IoIosArrowRoundForward } from "react-icons/io";
+import WhoWeAre from "../WhoWeAre/WhoWeAre";
 
 export default function ServicesHome() {
   const services = [
@@ -34,63 +35,69 @@ export default function ServicesHome() {
     },
   ];
   return (
-    <section className={styles.servicesAllDataContainer}>
-      <div className={styles.servicesTitleTextContainer}>
-        <h2 className={styles.servicesTitleText}>What We Have To Offer</h2>
+    <>
+      <section className={styles.servicesAllDataContainer}>
+        <div className={styles.servicesTitleTextContainer}>
+          <h2 className={styles.servicesTitleText}>What We Have To Offer</h2>
 
-        <p>
-          <Link className={styles.servicesViewAllServicesBtn} href={"/service"}>
-            View all Services
-            <MdArrowForward className={styles.servicesViewAllServicesIcon} />
-          </Link>
-        </p>
-      </div>
-
-      <div className={styles.servicesCardContainer}>
-        {services.map((service, ind) => (
-          <div
-            className={`${styles.servicesCard} ${
-              ind === 1 ? styles.servicesHighLightBg : ""
-            }`}
-            key={ind}
-          >
-            <Image
-              className={styles.servicesCardImage}
-              src={service?.image}
-              width={70}
-              height={70}
-            />
-            <h3
-              className={`${styles.servicesCardTitleText} ${
-                ind === 1 ? styles.servicesHighLightText : ""
-              }`}
-            >
-              {service?.service}
-            </h3>
-            <p
-              className={`${styles.servicesCardSubTitleText} ${
-                ind === 1 ? styles.servicesHighLightText : ""
-              }`}
-            >
-              {service?.title}
-            </p>
-
+          <p>
             <Link
-              className={`${styles.servicesReadMoreBtn} ${
-                ind === 1 ? styles.servicesReadMoreHighLightBtn : ""
-              }`}
-              href={service?.url}
+              className={styles.servicesViewAllServicesBtn}
+              href={"/service"}
             >
-              Read More
-              <IoIosArrowRoundForward
-                className={`${styles.servicesReadMoreBtnIcon} ${
-                  ind === 1 ? styles.servicesReadMoreHighLightBtnIcon : ""
-                }`}
-              />
+              View all Services
+              <MdArrowForward className={styles.servicesViewAllServicesIcon} />
             </Link>
-          </div>
-        ))}
-      </div>
-    </section>
+          </p>
+        </div>
+
+        <div className={styles.servicesCardContainer}>
+          {services.map((service, ind) => (
+            <div
+              className={`${styles.servicesCard} ${
+                ind === 1 ? styles.servicesHighLightBg : ""
+              }`}
+              key={ind}
+            >
+              <Image
+                className={styles.servicesCardImage}
+                src={service?.image}
+                width={70}
+                height={70}
+              />
+              <h3
+                className={`${styles.servicesCardTitleText} ${
+                  ind === 1 ? styles.servicesHighLightText : ""
+                }`}
+              >
+                {service?.service}
+              </h3>
+              <p
+                className={`${styles.servicesCardSubTitleText} ${
+                  ind === 1 ? styles.servicesHighLightText : ""
+                }`}
+              >
+                {service?.title}
+              </p>
+
+              <Link
+                className={`${styles.servicesReadMoreBtn} ${
+                  ind === 1 ? styles.servicesReadMoreHighLightBtn : ""
+                }`}
+                href={service?.url}
+              >
+                Read More
+                <IoIosArrowRoundForward
+                  className={`${styles.servicesReadMoreBtnIcon} ${
+                    ind === 1 ? styles.servicesReadMoreHighLightBtnIcon : ""
+                  }`}
+                />
+              </Link>
+            </div>
+          ))}
+        </div>
+      </section>
+      <WhoWeAre />
+    </>
   );
 }
