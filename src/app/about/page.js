652e8be33bfb4da94a7d02ace5d81a0e4d87d747
Aditto dev <computerscience01770@gmail.com>
2024-06-06 14:52:51 +1200

    [DONE]<About us sectiom add  and responsive>

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,15 +2,17 @@ import AboutHome from "@/modules/home/about";
 import styles from "./about.module.css";
 import TeamMembers from "@/modules/about/teamMembers";
 import Blog from "@/modules/home/blog";
-import WhyClientsLoveUs from "@/modules/about/whyClient’sLoveUs";
+// import WhyClientsLoveUs from "@/modules/about/whyClient’sLoveUs";
 import BrandPotential from "@/modules/about/brandPotential";
+import Feedback from "@/modules/home/feedback";
 
 export default function About() {
   return (
     <>
       <AboutHome />
       <TeamMembers />
-      <WhyClientsLoveUs />
+      {/* <WhyClientsLoveUs /> */}
+      <Feedback />
       <BrandPotential />
       {/* <Blog /> */}
     </>
