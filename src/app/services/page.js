fdf9b1ca59618127b3e5c263e83b4a7547b852e4
Aditto dev <computerscience01770@gmail.com>
2024-06-06 22:40:25 +1200

    [DONE]<Services section  add  and responsive>

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -2,12 +2,13 @@ import styles from "./services.module.css";
 import ServicesCategory from "@/modules/services/category";
 import RecentProject from "@/modules/services/recentProject";
 import LatestNews from "@/modules/services/latestNews";
+import WhoWeAre from "@/modules/home/WhoWeAre/WhoWeAre";
 
 export default function Services() {
   return (
     <>
       <ServicesCategory />
-      {/* <>Who we are component will there</> */}
+      <WhoWeAre />
       <RecentProject />
       <LatestNews />
     </>
